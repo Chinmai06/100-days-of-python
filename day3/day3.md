67e19c84e31d94b6b9ce1f5610589d749420c62b
Chinmai06 <chinmai.mandala96@gmail.com>
2026-05-28 00:10:23 -0400

    day3_edits

---
+++ day3/day3.md
@@ -0,0 +1,25 @@
+# Haunted House Escape
+
+Using Python, I created a simple text adventure program that asks the user to choose a direction and a route.
+
+- The user chooses left or right
+- If they choose right, they then choose between the basement and attic
+- If they choose the attic, they learn the correct door that contains the key
+- The program responds with a success or failure message based on the choice
+
+``` example
+Welcome to the Haunted House. To escape, you must find the hidden key!
+Which direction do you want to enter? Left press L or right press R: R
+Which route do you want to enter? Basement press B or Attic press A: A
+There is information for you: there are three doors. One leads to the kitchen, one to the bedroom, and one to the washroom. The key is in the kitchen.
+Which door do you want to open? Door 1 press D1, door 2 press D2, or door 3 press D3: D2
+Wrong door. You are haunted.
+Game over.
+```
+
+Methods used:
+
+1. Variables
+2. Input
+3. Conditional statements
+4. Print
--- day3/day3.py
+++ day3/day3.py
@@ -1,22 +1,23 @@
-print("Welcome to the Haunted House, To escape you must find the hidden key!")
-direction_choise=input("which direction do you want to enter? left press L or Right press R: ")
-if direction_choise=="L":
-	print("your haunted")
+print("Welcome to the Haunted House. To escape, you must find the hidden key!")
+direction_choice = input("Which direction do you want to enter? Left press L or right press R: ")
+if direction_choice == "L":
+    print("You are haunted.")
 else:
-	door_way_want_enter=input("enter the route you want to enter? basement press B or Arttic press A: ")
+    door_way_want_enter = input("Which route do you want to enter? Basement press B or Attic press A: ")
 
-	if(door_way_want_enter=="B"):
-		print("your haunted")
+    if door_way_want_enter == "B":
+        print("You are haunted.")
+    else:
+        print("There is information for you: there are three doors. One leads to the kitchen, one to the bedroom, and one to the washroom. The key is in the kitchen.")
+        door_you_want_to_open = input("Which door do you want to open? Door 1 press D1, door 2 press D2, or door 3 press D3: ")
+        if door_you_want_to_open == "D1":
+            print("Wrong door. You are haunted.")
+        elif door_you_want_to_open == "D2":
+            print("Wrong door. You are haunted.")
+        else:
+            print("You found the key. Go and open the door fast.")
 
-	else:
-	print("there is information for you: there are three doors one goes to kitchen, bedroom and washroom, but key is in kitchen")
-	door_you_want_to_open=input("which door you want to open? door1 press D1 or door2 press D2: ")
-	if (door_you_want_to_open=="D1"):
-		print("wrong door, your haunted")
-	elif(door_you_want_to_open=="D2")
-		print("wrong door, your haunted")
-	else:(door_you_want_to_open=="D3")
-		print("you got a key, go and open the door fast")
-print("game over")
+print("Game over.")
+
 
 
