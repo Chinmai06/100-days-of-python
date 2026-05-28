e82ae0b24f2deb5a35b957ea3340c26e58f21949
Chinmai06 <chinmai.mandala96@gmail.com>
2026-05-27 23:59:28 -0400

    day3

---
+++ day3/day3.py
@@ -0,0 +1,22 @@
+print("Welcome to the Haunted House, To escape you must find the hidden key!")
+direction_choise=input("which direction do you want to enter? left press L or Right press R: ")
+if direction_choise=="L":
+	print("your haunted")
+else:
+	door_way_want_enter=input("enter the route you want to enter? basement press B or Arttic press A: ")
+
+	if(door_way_want_enter=="B"):
+		print("your haunted")
+
+	else:
+	print("there is information for you: there are three doors one goes to kitchen, bedroom and washroom, but key is in kitchen")
+	door_you_want_to_open=input("which door you want to open? door1 press D1 or door2 press D2: ")
+	if (door_you_want_to_open=="D1"):
+		print("wrong door, your haunted")
+	elif(door_you_want_to_open=="D2")
+		print("wrong door, your haunted")
+	else:(door_you_want_to_open=="D3")
+		print("you got a key, go and open the door fast")
+print("game over")
+
+
